419478aed29eaf6b1f7ecfc0fef8328410e02c49
Oleksandra <photoliya@gmail.com>
2023-05-11 13:40:27 +0300

    task 1 / 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,23 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+
+const list = galleryItems.map((item) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+            </a>
+            </li>`;
+  }).join(" ");
+
+galleryList.insertAdjacentHTML("afterbegin", list);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
